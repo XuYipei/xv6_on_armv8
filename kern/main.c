b78ad0f4d4b403fdf3b5b3e9caf3c3fbb5213b80
XuYipei <1449097870@qq.com>
2020-10-23 14:48:45 +0800

    blabla

--- kern/main.c
+++ kern/main.c
@@ -29,8 +29,22 @@ main()
 
     irq_init();
 
+<<<<<<< HEAD
     lvbar(vectors);
     timer_init();
+=======
+    /*
+     * Modify ttbr0_el1 into a new user space pointer ttbr.
+     * In ttbr, va=0x0000000000000000 points to pa=0x0000000000001000.
+     * Therefore, if we load the data in va=0x0000000000000000, we can get 233.
+    */
+    uint64_t *ttbr = kalloc();
+    memset((char *)ttbr, 0, PGSIZE);
+    map_region(ttbr, 0, PGSIZE, 0x1000, 0);
+    modify_ttbr_el1(ttbr, 0);
+    va = 0x0000000000000010;
+    cprintf("%llu\n", *va);
+>>>>>>> 106b992... rebase dev2
 
     while (1) ;
 }
