680ca258d50094e3cc6696b6cd068f02173f36b5
XuYipei <1449097870@qq.com>
2020-10-23 14:27:43 +0800

    rebase dev2 -> dev

--- inc/uart.h
+++ inc/uart.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef INC_UART_H
 #define INC_UART_H
 
@@ -6,3 +7,13 @@ void uart_putchar(int);
 char uart_getchar();
 
 #endif
+=======
+#ifndef INC_UART_H
+#define INC_UART_H
+
+void uart_init(void);
+void uart_putchar(int);
+char uart_getchar();
+
+#endif
+>>>>>>> ed2d85b... Lab1, main.c
--- kern/main.c
+++ kern/main.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdint.h>
 
 #include "string.h"
@@ -86,3 +87,28 @@ main()
 
     while (1) ;
 }
+=======
+#include <stdint.h>
+
+#include "string.h"
+#include "console.h"
+
+void
+main()
+{
+    /*
+     * Before doing anything else, we need to ensure that all
+     * static/global variables start out zero.
+     */
+
+    extern char edata[], end[];
+
+    /* TODO: Use `memset` to clear the BSS section of our program. */
+    memset(edata, 0, end - edata);
+    /* TODO: Use `cprintf` to print "hello, world\n" */
+    console_init();
+    cprintf("hello, world\n");
+
+    while (1) ;
+}
+>>>>>>> ed2d85b... Lab1, main.c
