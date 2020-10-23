69130e96589ab64db9ad79f5ddae3347b594a01e
XuYipei <1449097870@qq.com>
2020-10-23 14:47:46 +0800

    blabla

--- kern/main.c
+++ kern/main.c
@@ -4,40 +4,8 @@
 #include "string.h"
 #include "console.h"
 #include "kalloc.h"
-<<<<<<< HEAD
 #include "trap.h"
 #include "timer.h"
-=======
-#include "vm.h"
-#include "mmu.h"
-
-extern char end[];
-
-static inline uint64_t
-load_ttbr_el1(uint32_t sp)
-{
-    // asm volatile("mrs vbar_el1, %[1]": [x]"=r"(p) :);
-    uint64_t ttbr = 0;
-    if (sp == 0)
-        asm volatile("mrs %[dest], ttbr0_el1": [dest]"=r"(ttbr) : );
-    if (sp == 1)
-        asm volatile("mrs %[dest], ttbr1_el1": [dest]"=r"(ttbr) : );
-    return(ttbr);
-}
-
-static inline void
-modify_ttbr_el1(uint64_t *ttbr, uint32_t sp)
-{  
-    if (sp == 0)
-        asm volatile("msr ttbr0_el1, %[src]": : [src]"r"(ttbr));
-    if (sp == 1)
-        asm volatile("msr ttbr1_el1, %[src]": : [src]"r"(ttbr));
-    
-    asm volatile("ic iallu": : );
-    asm volatile("tlbi vmalle1is": : );
-    asm volatile("dsb ish": : );
-}
->>>>>>> 1c76857... lab 2
 
 void
 main()
