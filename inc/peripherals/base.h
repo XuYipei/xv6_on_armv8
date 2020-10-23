f8798fe6e07ce69572a494aefe5cb4e5a546ae5c
XuYipei <1449097870@qq.com>
2020-10-23 14:55:52 +0800

    wtf?

--- inc/peripherals/base.h
+++ inc/peripherals/base.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef INC_PERIPHERALS_BASE_H
 #define INC_PERIPHERALS_BASE_H
 
@@ -8,13 +7,3 @@
 #define LOCAL_BASE  (KERNBASE + 0x40000000)
 
 #endif
-=======
-#ifndef INC_PERIPHERALS_BASE_H
-#define INC_PERIPHERALS_BASE_H
-
-#include "memlayout.h"
-
-#define MMIO_BASE (KERNBASE + 0x3F000000)
-
-#endif
->>>>>>> 680ca25... rebase dev2 -> dev
