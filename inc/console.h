36f2007fcae1e47aa4282b2a671030af488aafe0
XuYipei <1449097870@qq.com>
2020-10-23 14:48:22 +0800

    lab7

--- inc/console.h
+++ inc/console.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef INC_CONSOLE_H
 #define INC_CONSOLE_H
 
@@ -27,47 +26,3 @@ void panic(const char *fmt, ...);
 })
 
 #endif
-=======
-#ifndef INC_CONSOLE_H
-#define INC_CONSOLE_H
-
-#include <stdarg.h>
-#include <stdint.h>
-
-void console_init();
-void cprintf(const char *fmt, ...);
-void panic(const char *fmt, ...);
-
-#define assert(x)                                                   \
-({                                                                  \
-    if (!(x)) {                                                     \
-        cprintf("%s:%d: assertion failed.\n", __FILE__, __LINE__);  \
-        while(1) ;                                                  \
-    }                                                               \
-})
-
-#endif
-<<<<<<< HEAD
-=======
-=======
-#ifndef INC_CONSOLE_H
-#define INC_CONSOLE_H
-
-#include <stdarg.h>
-#include <stdint.h>
-
-void console_init();
-void cprintf(const char *fmt, ...);
-void panic(const char *fmt, ...);
-
-#define assert(x)                                                   \
-({                                                                  \
-    if (!(x)) {                                                     \
-        cprintf("%s:%d: assertion failed.\n", __FILE__, __LINE__);  \
-        while(1) ;                                                  \
-    }                                                               \
-})
-
-#endif
->>>>>>> 4007444... lab5
->>>>>>> 680ca25... rebase dev2 -> dev
