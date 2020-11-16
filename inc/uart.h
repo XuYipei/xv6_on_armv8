99e3678a022f6cc6846995e0f0b33754eb9c8aa6
XuYipei <1449097870@qq.com>
2020-11-16 18:37:44 +0800

    lab5 upd

--- inc/uart.h
+++ inc/uart.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef INC_UART_H
 #define INC_UART_H
 
@@ -7,3 +8,13 @@ void uart_putchar(int);
 char uart_getchar();
 
 #endif
+=======
+#ifndef INC_UART_H
+#define INC_UART_H
+
+void uart_init(void);
+void uart_putchar(int);
+char uart_getchar();
+
+#endif
+>>>>>>> ed2d85b... Lab1, main.c
