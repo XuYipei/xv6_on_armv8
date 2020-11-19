947ec1929ad52fc8b3ce8947332e7e9fd7a8a18c
XuYipei <1449097870@qq.com>
2020-11-19 11:10:58 +0800

    cpus

--- kern/trap.c
+++ kern/trap.c
@@ -12,13 +12,26 @@
 #include "timer.h"
 #include "proc.h"
 
+uint32_t irqinitcnt;
+struct spinlock irqinitlock;
+
 void
 irq_init()
 {
+    acquire(&irqinitlock);
+
+    if (irqinitcnt){
+        release(&irqinitlock);
+        return;
+    }
+    irqinitcnt = 1;
+
     cprintf("irq_init: - irq init\n");
     clock_init();
     put32(ENABLE_IRQS_1, AUX_INT);
     put32(GPU_INT_ROUTE, GPU_IRQ2CORE(0));
+
+    release(&irqinitlock);
 }
 
 void
