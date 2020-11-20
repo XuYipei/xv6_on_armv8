52bf1ec79a997a2771c0eb657c224a6e4fee5448
XuYipei <1449097870@qq.com>
2020-11-20 10:56:36 +0800

    lab5

--- kern/spinlock.c
+++ kern/spinlock.c
@@ -44,23 +44,13 @@ release(struct spinlock *lk)
     __atomic_clear(&lk->locked, __ATOMIC_RELEASE);
 }
 
-<<<<<<< HEAD
-=======
-struct mcslock zero = {(struct mcslock *)NULL, 0};
-
-
->>>>>>> e06d4a5... pgdr
 void
 mcsacquire(struct mcslock *lk, struct mcslock *i){
     i->locked = 0;
     i->next = NULL;
-<<<<<<< HEAD
     struct mcslock *tail = lk->next;
     while (!__atomic_compare_exchange_n(&lk->next, &tail, i, 0, __ATOMIC_RELEASE, __ATOMIC_ACQUIRE))
         tail = lk->next;
-=======
-    struct mcslock *tail = __sync_lock_test_and_set(&lk->next, i, __ATOMIC_ACQUIRE);
->>>>>>> e06d4a5... pgdr
     if (tail){
         i->locked = 1;
         tail->next = i;
@@ -70,9 +60,7 @@ mcsacquire(struct mcslock *lk, struct mcslock *i){
 }
 
 void 
-<<<<<<< HEAD
 mcsrelease(struct mcslock *lk, struct mcslock *i){
-<<<<<<< HEAD
     struct mcslock *x = i;
     if (__atomic_compare_exchange_n(&lk->next, &x, NULL, 0, __ATOMIC_RELEASE, __ATOMIC_ACQUIRE))
         return;
@@ -80,39 +68,3 @@ mcsrelease(struct mcslock *lk, struct mcslock *i){
         ;   
     i->next->locked = 0;
 }
-=======
-=======
-mrelease(struct mcslock *lk, struct mcslock *i){
->>>>>>> e62f6d8... clhlock
-    if (i->next == NULL){
-        if (__sync_val_compare_and_swap(&lk->next, i, NULL)) 
-            return;
-    }
-    while (i->next == 0) 
-        ;
-    i->next->locked = 0;
-<<<<<<< HEAD
-}
->>>>>>> e06d4a5... pgdr
-=======
-    free(i);
-}
-
-
-void
-macquire(struct clhlock *lk, struct clhlock *i){
-    lk->locked = 1;
-    i->prev = __sync_lock_test_and_set(&lk->prev, i, __ATOMIC_ACQUIRE);
-    while (__atomic_load_n(&i->prev->locked))
-        ;
-}
-
-void 
-mrelease(struct clhlock *lk, struct clhlock *i){
-    if (i->prev){
-        free(i->prev);
-        i->prev = (struct clhlock *)0;
-    }
-    __atomic_store_n(i->locked, 0, __ATOMIC_RELEASE);
-}
->>>>>>> e62f6d8... clhlock
