54d1975ca8b66d7c392c21ef457f423cd0b8e556
XuYipei <1449097870@qq.com>
2020-12-04 16:19:16 +0800

    rebase

--- inc/console.h
+++ inc/console.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef INC_CONSOLE_H
 #define INC_CONSOLE_H
 
@@ -26,3 +27,24 @@ void panic(const char *fmt, ...);
 })
 
 #endif
+=======
+#ifndef INC_CONSOLE_H
+#define INC_CONSOLE_H
+
+#include <stdarg.h>
+#include <stdint.h>
+
+void console_init();
+void cprintf(const char *fmt, ...);
+void panic(const char *fmt, ...);
+
+#define assert(x)                                                   \
+({                                                                  \
+    if (!(x)) {                                                     \
+        cprintf("%s:%d: assertion failed.\n", __FILE__, __LINE__);  \
+        while(1) ;                                                  \
+    }                                                               \
+})
+
+#endif
+>>>>>>> 680ca25... rebase dev2 -> dev
